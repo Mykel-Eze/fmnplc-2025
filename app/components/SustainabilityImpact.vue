da1794a9e4e3eb0b6256a70eaad284ec72011ccb
Mykel-Eze <michaeleze1997@gmail.com>
2025-10-26 19:50:15 +0100

    minor alignment issue fixed

--- app/components/SustainabilityImpact.vue
+++ app/components/SustainabilityImpact.vue
@@ -1,6 +1,6 @@
 <template>
   <section id="sustainability-impact" class="impact-section py-20 px-4 sm:px-6 lg:px-8">
-    <div class="max-w-7xl mx-auto">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="flex flex-col lg:flex-row items-center gap-12 lg:gap-16">
         <!-- Left Side - Image Group -->
         <div class="w-full lg:w-1/2">
