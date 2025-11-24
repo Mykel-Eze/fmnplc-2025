9df1e209d1878479099e5de932ad3f39ed446a46
Mykel-Eze <michaeleze1997@gmail.com>
2025-11-24 04:57:13 +0100

    Managment updates

--- app/pages/about/management.vue
+++ app/pages/about/management.vue
@@ -10,7 +10,7 @@ const managementTeam = [
   {
     id: '1',
     name: 'Omoboyede Olusanya',
-    position: 'Group Managing Director & CEO',
+    position: 'Group Chief Executive Officer',
     image: '/team/omoboyede.jpg'
   },
   {
@@ -27,42 +27,24 @@ const managementTeam = [
   },
   {
     id: '4',
-    name: 'Stoney Su',
-    position: 'MD Agro-Allied Division',
-    image: '/team/stoney.jpg'
-  },
-  {
-    id: '5',
-    name: 'Anders Kristiansson',
-    position: 'Group Chief Finance Officer',
-    image: '/team/anders.jpg'
-  },
-  {
-    id: '6',
     name: 'Uchenna S. Ajo',
     position: 'Group HR & Services Director',
     image: '/team/uchenna.jpg'
   },
   {
-    id: '7',
+    id: '5',
     name: 'Joseph Umolu',
     position: 'Company Secretary & Group Director, Legal Services',
     image: '/team/joseph.jpg'
   },
   {
-    id: '8',
-    name: 'Bola Adeeko',
-    position: 'Group Director, Special Projects',
-    image: '/team/bola.jpg'
-  },
-  {
-    id: '9',
+    id: '6',
     name: 'Sadiq Usman',
-    position: 'Group Director, Strategy & Stakeholder Relations',
+    position: 'MD, FMN AGRO and Group Director, Partnerships and Strategic Engagements',
     image: '/team/sadiq.jpg'
   },
   {
-    id: '10',
+    id: '7',
     name: 'Cephas Afebuameh',
     position: 'Group Director, Supply Chain',
     image: '/team/cephas.jpg'
